924e014e75a040f72297368dfaee0abe23a88f90
patrikbujna <bujnapato@icloud.com>
2018-03-28 00:46:35 +0200

    done

--- app.py
+++ app.py
@@ -11,13 +11,15 @@ def vystup():
     skratkaGoly = False
     skratkaZostavy = False
 
+    '''
     if 'checkbox' in request.form:
         if request.form['checkbox'] == 'skratkaGoly':
             skratkaGoly = True
         if request.form['checkbox'] == 'skratkaZostavy':
             skratkaZostavy = True
+    '''
 
-    return render_template('vystup.html', vystup = main.getStringVystup(url, liga, skratkaGoly, skratkaZostavy))
+    return render_template('vystup.html', vystup = main.getStringVystup(url, liga, True, True))
 
 @app.route('/')
 def index():
